4eeae78ab897b362b18cb0d6fa42c8256ff89cec
Cpache1 <capacheco111@outlook.com>
2021-04-24 20:17:40 +0100

    Merge?

---
+++ Assets/Carlos/Scripts/ColorSyncTest_Carlos.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSyncTest_Carlos : MonoBehaviour
+{
+    [SerializeField]
+    private Color _color;
+    private Color _previousColor = default;
+
+    private ColorSync_Carlos _colorSync;
+
+    [SerializeField]
+    private float _speed = 0.1f;
+
+    [SerializeField]
+    private Quaternion _rotation = default;
+    private Quaternion _previousRotation = default;
+
+    private void Awake()
+    {
+        // Get a reference to the color sync component
+        _colorSync = GetComponent<ColorSync_Carlos>();
+    }
+
+    private void Update()
+    {
+        // If the color has changed (via the inspector), call SetColor on the color sync component.
+        if (_color != _previousColor)
+        {
+            _colorSync.SetColor(_color);
+            _previousColor = _color;
+        }
+
+        // Update rotation is changed via the inspector
+        if (_rotation != _previousRotation)
+        {
+            _colorSync.SetRotation(_rotation);
+            _previousRotation = _rotation;
+        }
+
+        // Update position with wasd and scale with qe
+        if (Input.GetKey(KeyCode.W))
+        {
+            _colorSync.SetPosition(_colorSync.transform.position += Vector3.up * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _colorSync.SetPosition(_colorSync.transform.position += Vector3.left * _speed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _colorSync.SetPosition(_colorSync.transform.position += Vector3.down * _speed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _colorSync.SetPosition(_colorSync.transform.position += Vector3.right * _speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            _colorSync.SetScale(_colorSync.transform.localScale += Vector3.down * _speed * Time.deltaTime);
+
+
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            _colorSync.SetScale(_colorSync.transform.localScale += Vector3.up * _speed * Time.deltaTime);
+
+
+        }
+
+    }
+
+
+}
